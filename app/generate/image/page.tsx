7d541b63059a129ab31e784051dde862b4e2f36b
sohanemon <m.sohanemon@gmail.com>
2023-02-25 23:20:44 +0600

    all refs removed from mt

--- app/generate/image/page.tsx
+++ app/generate/image/page.tsx
@@ -2,31 +2,24 @@
 import { changeInput } from '@/services/redux/slices/image-prompt';
 import { RootState } from '@/services/redux/store';
 import { Button, Input } from '@material-tailwind/react';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, ReactElement, FormEvent } from 'react';
 import { BiMessageRoundedDots } from 'react-icons/bi';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function Page() {
   const dispatch = useDispatch();
-  const inputRef = useRef<any>(null);
-  let inputElement: any;
-  useEffect(() => {
-    inputElement = inputRef.current.querySelector('input');
-    return () => {};
-  }, []);
 
   const inputState = useSelector(
     (state: RootState) => state['image-prompt'].inputValue
   );
+  console.log('🛑 ~ Page ~ inputState:', inputState);
 
+  /* ------------ adding event listener for material tailwind ------------ */
   /* ----------------------------- functions ----------------------------- */
-  function handleSubmit() {
-    dispatch(changeInput(inputElement?.value));
-    console.log('🛑 ~ Page ~ inputState:', inputState);
-  }
+  function handleSubmit() {}
 
-  function handleInputChange() {
-    dispatch(changeInput(inputElement.value));
+  function handleInputChange(e: React.FormEvent<HTMLInputElement>) {
+    console.log((e.target as HTMLInputElement).value);
   }
 
   /* --------------------------------------------------------------------- */
@@ -34,8 +27,7 @@ export default function Page() {
     <div className='grid min-h-screen place-content-center'>
       <div className='w-96'>
         <Input
-          onInput={handleInputChange}
-          ref={inputRef}
+          onInput={(e) => handleInputChange(e)}
           color='gray'
           label='Write prompt'
           icon={<BiMessageRoundedDots />}
