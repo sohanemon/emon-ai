5824dae384955f1da96c627be7d49d31ec6e4c4c
sohanemon <m.sohanemon@gmail.com>
2023-02-26 00:58:43 +0600

    bringged to fronted only

--- app/api/openai/generate-image/route.ts
+++ app/api/openai/generate-image/route.ts
@@ -1,22 +1,4 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { Configuration, OpenAIApi } from 'openai';
 
-/* ---------------------- setting up configuration --------------------- */
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-/* ---------------------- creating openai instance --------------------- */
-const openai = new OpenAIApi(configuration);
-
-export async function GET() {
-  try {
-    const result = await openai.createImage({
-      prompt: 'beautiful girl with face',
-      n: 1,
-      size: '1024x1024',
-    });
-    return new Response(result.data.data[0].url);
-  } catch (error) {
-    console.log('🛑 ~ GET ~ error:', error);
-  }
-}
+export async function GET(req: Request) {}
