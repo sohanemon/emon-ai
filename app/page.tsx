683ffb7182364de841e391a66863cbd29405e79f
sohanemon <m.sohanemon@gmail.com>
2023-02-25 18:14:03 +0600

    package updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/button';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -5,10 +6,9 @@ const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
   return (
     <main style={inter.style} className='min-h-screen text-white bg-black'>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
-      molestias rem maxime adipisci asperiores consectetur laborum delectus nemo
-      enim, corporis quos assumenda alias eligendi, sapiente commodi eaque, in
-      necessitatibus nostrum.
+      <div className='grid min-h-screen place-content-center'>
+        <Button>Image Generation</Button>
+      </div>
     </main>
   );
 }
