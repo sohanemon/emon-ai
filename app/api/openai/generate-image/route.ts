84fde2a5ab3ec06592f18698b835bb0679752539
sohanemon <m.sohanemon@gmail.com>
2023-02-25 17:34:56 +0600

    openai configured

--- app/api/openai/generate-image/route.ts
+++ app/api/openai/generate-image/route.ts
@@ -1,7 +1,13 @@
 import { Configuration, OpenAIApi } from 'openai';
+
+/* ---------------------- setting up configuration --------------------- */
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+/* ---------------------- creating openai instance --------------------- */
+const openai = new OpenAIApi(configuration);
+
 export async function GET() {
   return new Response(
     JSON.stringify({
