d59a9d6fc29e8b05877ea4e456a969a2e4911dd7
sohanemon <m.sohanemon@gmail.com>
2023-02-27 00:04:19 +0600

    long answer generated

--- utils/fetch-answer.ts
+++ utils/fetch-answer.ts
@@ -3,8 +3,12 @@ import openai from '@/lib/open-ai';
 export default async function fetchAnswer() {
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: 'what is javascript',
-    temperature: 0,
+    prompt: 'explain this step by step: what is javascript',
+    temperature: 0.9,
+    max_tokens: 1000,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    top_p: 1,
   });
 
   return completion.data.choices[0].text;
