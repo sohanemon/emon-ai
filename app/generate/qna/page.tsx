9e3b935485948c14d3e676d51b8a0bd407925407
sohanemon <m.sohanemon@gmail.com>
2023-02-26 23:14:10 +0600

    text box added

--- app/generate/qna/page.tsx
+++ app/generate/qna/page.tsx
@@ -1,13 +1,20 @@
 'use client';
-import { Button, Input } from '@material-tailwind/react';
+import { Button, Input, Textarea } from '@material-tailwind/react';
 export default function Page() {
   return (
     <>
       <div className='grid justify-center pt-10'>
-        <div className='w-96'>
-          <Input
+        <div className='w-96 h-[calc(100vh-120px)] flex flex-col justify-end'>
+          <div className='grow '>
+            <span>{'> '}</span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus,
+            laborum fuga? Ullam ducimus molestiae, animi autem maiores ut sit
+            earum praesentium adipisci, ipsa provident dicta qui corporis minus?
+            Accusantium, architecto?
+          </div>
+          <Textarea
             color='gray'
-            label={'label'}
+            label={'Ask your question'}
 
             // icon={
             //   !inputLength ? (
