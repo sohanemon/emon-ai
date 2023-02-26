2f94548d5e75f68833888046d320a3eb898696dc
sohanemon <m.sohanemon@gmail.com>
2023-02-27 00:38:04 +0600

    front page updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <div className='grid min-h-screen place-content-center'>
+      <div className='flex flex-col min-h-screen place-content-center place-items-center gap-y-3'>
         <Link href={'generate/image'}>
           <Button>Image Generation</Button>
+        </Link>
+        <Link href={'generate/qna'} className='block'>
+          <Button>Question/Answer</Button>
         </Link>
       </div>
     </main>
